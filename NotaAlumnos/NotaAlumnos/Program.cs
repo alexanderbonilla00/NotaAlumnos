31669d36ba57741ceb37414a6b9b77cf5bad753c
alexanderbonilla00 <deleonbonillaalexander@gmail.com>
2017-07-08 19:16:46 -0700

    Finalización de Proyecto

--- NotaAlumnos/NotaAlumnos/Program.cs
+++ NotaAlumnos/NotaAlumnos/Program.cs
@@ -10,7 +10,57 @@ namespace NotaAlumnos
     {
         static void Main(string[] args)
         {
+            int cMaterias = 0;
 
+            Console.WriteLine("Cuantas materias Tiene: ");
+            cMaterias = Convert.ToInt32(Console.ReadLine());
+
+            if (cMaterias <= 0)
+            {
+                Console.WriteLine("Cantidad de Materias no Válidas");
+
+            }
+
+            float[] nota = new float[cMaterias];
+            float[] creditos = new float[cMaterias];
+            string[] materia = new string[cMaterias];
+            int contador = 0;          
+            float tCreditos = 0.0f;
+            float tNotas = 0.0f;
+            float promedio = 0.0f;
+
+            for (contador = 0; contador <cMaterias; contador++)
+            {
+                Console.WriteLine("Introduzca El Nombre de la materia #{0}", contador + 1);
+                materia[contador] = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Introduzca la nota de la materia #{0}", contador + 1);
+                nota[contador] = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Introduzca los Creditos de la materia #{0}", contador + 1);
+                creditos[contador] = Convert.ToSingle(Console.ReadLine());
+            }
+            contador = 0;
+            for (contador = 0; contador <cMaterias; contador++)
+            {
+                tNotas+= nota[contador] * creditos[contador];
+                tCreditos+= creditos[contador];
+            }
+
+            promedio += (tNotas / tCreditos);
+
+            contador = 0;
+            Console.Clear();
+
+            for (contador = 0; contador <cMaterias; contador++)
+            {
+                Console.Write(materia[contador]);
+                Console.Write(": ");
+                Console.Write(nota[contador]);
+                Console.Write(", Creditos: ");
+                Console.WriteLine(creditos[contador]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Su promedio es: {0}", promedio);
         }
+
     }
 }
